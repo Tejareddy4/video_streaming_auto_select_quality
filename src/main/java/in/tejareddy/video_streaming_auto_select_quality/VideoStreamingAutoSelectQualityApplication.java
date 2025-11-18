22d90beab42838d258409fbea7733c4b0d53d9c6
Teja Reddy <76819847+Tejareddy4@users.noreply.github.com>
2025-11-18 15:38:47 +0530

    Project local created

---
+++ src/main/java/in/tejareddy/video_streaming_auto_select_quality/VideoStreamingAutoSelectQualityApplication.java
@@ -0,0 +1,13 @@
+package in.tejareddy.video_streaming_auto_select_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingAutoSelectQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingAutoSelectQualityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/tejareddy/video_streaming_auto_select_quality/VideoStreamingAutoSelectQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tejareddy.video_streaming_auto_select_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingAutoSelectQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
