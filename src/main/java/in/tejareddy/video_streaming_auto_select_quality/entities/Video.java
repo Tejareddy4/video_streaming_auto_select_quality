743f6f6fdc5c9fb7b4a04ac83d473bbdc5a1ef27
Teja Reddy <76819847+Tejareddy4@users.noreply.github.com>
2025-11-18 16:33:54 +0530

    connect database and added entity

---
+++ src/main/java/in/tejareddy/video_streaming_auto_select_quality/entities/Video.java
@@ -0,0 +1,23 @@
+package in.tejareddy.video_streaming_auto_select_quality.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name ="videos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String title;
+    private String description;
+    private String author;
+    private String contentType;
+    private String filePath;
+
+}
